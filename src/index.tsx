8fe54c6ec0904ad62917e6e571e3790e0b944dea
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-07-18 15:31:27 +0300

    added App to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+const App = () => {
+    return <></>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)

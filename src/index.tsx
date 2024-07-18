ba4d165aaea61f98c9673a6e22e77dffaf239c3e
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-07-18 15:57:50 +0300

    added header.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-const App = () => {
-    return <></>
-}
+import App from './layout/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(

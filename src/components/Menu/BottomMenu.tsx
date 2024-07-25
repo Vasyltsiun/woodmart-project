3b7428d0f54ae83b6fc4d7897aef134f0d27fc91
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-07-25 12:42:41 +0300

    fixed href attribute in BottomMenu.tsx

--- src/components/Menu/BottomMenu.tsx
+++ src/components/Menu/BottomMenu.tsx
@@ -6,38 +6,40 @@ const BottomMenu = (props: Props) => {
         <>
             <ul className="bottom_menu">
                 <li className="chairs">
-                    <a href="">Chairs</a>
+                    <a href="chairs">Chairs</a>
                 </li>
                 <li className="tablets">
-                    <a href="">Tablets</a>
+                    <a href="tablets">Tablets</a>
                 </li>
                 <li className="sofas">
-                    <a href="">Sofas</a>
+                    <a href="sofas">Sofas</a>
                 </li>
                 <li className="armchairs">
-                    <a href="">Armchairs</a>
+                    <a href="armchairs">Armchairs</a>
                 </li>
                 <li className="beds">
-                    <a href="">Beds</a>
+                    <a href="beds">Beds</a>
                 </li>
                 <li className="storage">
-                    <a href="">Storage</a>
+                    <a href="storage">Storage</a>
                 </li>
                 <li className="textiles">
-                    <a href="">Textiles</a>
+                    <a href="textiles">Textiles</a>
                 </li>
                 <li className="lighting">
-                    <a href="">Lighting</a>
+                    <a href="lighting">Lighting</a>
                 </li>
                 <li className="toys">
-                    <a href="">Toys</a>
+                    <a href="toys">Toys</a>
                 </li>
                 <li className="decor">
-                    <a href="">Decor</a>
+                    <a href="decor">Decor</a>
                 </li>
             </ul>
             <div className="btn_free_shipping">
-                <a href="">Free shipping for all orders of $1.300</a>
+                <a href="free_shipping">
+                    Free shipping for all orders of $1.300
+                </a>
             </div>
         </>
     )

2c850307ac5d0005d9b2946211f671a0f5360d68
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-08-28 19:23:04 +0300

    fixed

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -3,8 +3,9 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Footer from './Footer'
 import { Route, Routes } from 'react-router'
-import Chairs from 'pages/Chairs/ChairsPage'
-import Home from 'pages/Home/HomePage'
+
+import ChairsPage from 'pages/Chairs/ChairsPage'
+import HomePage from 'pages/Home/HomePage'
 import TabletsPage from 'pages/Tablets/TabletsPage'
 
 const App = () => {
@@ -13,8 +14,8 @@ const App = () => {
             <CssBaseline />
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/chairs" element={<Chairs />} />
+                <Route path="/" element={<HomePage />} />
+                <Route path="/chairs" element={<ChairsPage />} />
                 <Route path="/tablets" element={<TabletsPage />} />
             </Routes>
             <Footer />

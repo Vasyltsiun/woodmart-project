552d33265a95302f78ec15690b2cca45a6452aa7
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-08-31 15:43:08 +0300

    fixed slider menu

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -3,24 +3,25 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
 import Footer from './Footer'
 import { Route, Routes } from 'react-router'
-
 import ChairsPage from 'pages/Chairs/ChairsPage'
 import HomePage from 'pages/Home/HomePage'
 import TabletsPage from 'pages/Tablets/TabletsPage'
+import SliderMain from 'components/SliderMain/SliderMain'
 
 const App = () => {
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
             <Header />
+            <SliderMain />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route
-                    path="chairs"
+                    path="/chairs"
                     element={<ChairsPage category="chairs" />}
                 />
                 <Route
-                    path="tablets"
+                    path="/tablets"
                     element={<TabletsPage category="tablets" />}
                 />
             </Routes>
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,12 +1,10 @@
 import { StyledEngineProvider } from '@mui/material'
-import SliderMain from 'components/SliderMain/SliderMain'
 import Main from 'layout/Main'
 
 type Props = {}
 const HomePage = (props: Props) => {
     return (
         <StyledEngineProvider injectFirst>
-            <SliderMain />
             <Main />
         </StyledEngineProvider>
     )

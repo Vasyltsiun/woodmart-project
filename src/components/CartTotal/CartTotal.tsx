300b5bbf33b235bde68f8db7da6ffc6d61a20bde
Oleksandr <vasiltsun.oleksandr@gmail.com>
2024-09-23 14:14:15 +0300

    added cart total content

--- src/components/CartTotal/CartTotal.tsx
+++ src/components/CartTotal/CartTotal.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material'
 import { getProductsObject, Product, productsArrey } from 'utils/productsArrey'
 
 type Props = {
@@ -13,18 +14,31 @@ const CartTotal = ({
     productsObject = getProductsObject(productsArrey),
 }: Props) => {
     return (
-        <div>
-            <div>
-                Total:{' '}
-                {Object.keys(productsInCart).reduce(
-                    (total, productId) =>
-                        total +
-                        productsInCart[+productId] *
-                            productsObject[+productId].price,
-                    0
-                )}
-                $
+        <div className="cart_content">
+            {' '}
+            Cart Totals
+            <div className="cart_shipping">
+                <div className="shipping">Shipping</div>
+                <div className="shipping_content">
+                    <div className="free_shipping">Free shipping</div>
+                    <div className="shipping_all">Shipping to AL.</div>
+                    <div className="change_address">Change address</div>
+                </div>
             </div>
+            <div className="cart_totals">
+                <div className="cart_name">Total</div>
+                <div className="cart_sum">
+                    {Object.keys(productsInCart).reduce(
+                        (total, productId) =>
+                            total +
+                            productsInCart[+productId] *
+                                productsObject[+productId].price,
+                        0
+                    )}
+                    $
+                </div>
+            </div>
+            <Button className="btns_wrapper">Proceed to checkout</Button>
         </div>
     )
 }
